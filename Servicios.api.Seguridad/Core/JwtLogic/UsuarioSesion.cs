a4ffcb7cfe0b7be7f0f40ad9f2340db386778394
Armin Cetina Mac <ext.armin.cetina@pricetravel.com>
2023-09-22 21:38:13 -0500

    Initial commit

---
+++ Servicios.api.Seguridad/Core/JwtLogic/UsuarioSesion.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Linq;
+
+namespace Servicios.api.Seguridad.Core.JwtLogic
+{
+    public class UsuarioSesion : IUsuarioSesion
+    {
+        private IHttpContextAccessor _httpContextAccessor;
+
+        public UsuarioSesion(IHttpContextAccessor httpContextAccessor) {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+
+        public string GetUsuarioSesion()
+        {
+            var userName = _httpContextAccessor.HttpContext.User?.Claims?.FirstOrDefault(x => x.Type == "username")?.Value;
+            return userName;
+        }
+    }
+}
